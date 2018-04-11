c18e56a011f0d4c496ce3ba5e6adbd634abc5d0e
MargaritaNik <mapgosha98@yandex.ru>
2018-04-12 03:39:45 +0500

    fix

--- hw3(2).c
+++ hw3(2).c
@@ -11,14 +11,14 @@ char *strncat(char *original, char *append, size_t n)
         if (!(*original++ = *append++)){
             return ret;	}
     }
-
+    *original = 0;
     return ret;
 }
 int main (void) {
 	
 	char original[10]="Game Of";
     char append[]=" Throns";
-    strncat(original, append, 15);
+    strncat(original, append, 5);
     printf("%s\n", original);
 
 }
